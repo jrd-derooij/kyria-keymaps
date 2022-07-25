b11be15c3217999bf9babc567d46c2b03c2d8712
jrd-derooij <jrd.derooij@gmail.com>
2022-07-25 22:32:35 +0200

    .

--- keymap.c
+++ keymap.c
@@ -6,7 +6,8 @@ enum layers {
 	_NAVIGATION,
 	_NUMBERS,
 	_FUNCTION,
-    _RGB
+    _RGB,
+    _COLEMAK_DH
 };
 
 #define CTL_ESC     MT(MOD_LCTL, KC_ESC)
@@ -48,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 									 	  			      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   _______, 
 		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,								 	  			      KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_ENT, _______, 
 		_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  _______, _______, 		        _______, _______, KC_UNDSC,KC_LCBR, KC_RCBR, KC_RCBR, _______, _______, 
-						           _______, _______, _______, _______, TG(_FUNCTION), 	        _______, _______, _______, _______, TO(_RGB)
+						           TO(_COLEMAK_DH), _______, _______, _______, TG(_FUNCTION), 	        _______, _______, _______, _______, TO(_RGB)
 	),
     [_NAVIGATION] = LAYOUT(
       _______, _______, _______, _______, _______, _______,                                              KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
@@ -87,6 +88,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, 	DF(_NAVIGATION), _______, _______, _______, _______, 		  	_______, _______, 	_______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD, 	_______, 
 									_______, 		_______, _______, _______, _______, 			_______, _______, 	_______, _______, TO(_BASE)
 	),
+    [_COLEMAK_DH] = LAYOUT(
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSLS,
+     CTL_ESC , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
+	 	        KC_LCTL, KC_LALT, KC_LGUI, LT(_NAVIGATION, KC_SPC), KC_MEH,       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
+    ),
+
 };
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
