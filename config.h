c3b7effaf2b18f517ebe4349d3b27b4c16c78c64
jrd-derooij <jrd.derooij@gmail.com>
2022-07-26 20:42:52 +0200

    Auto Commit by copy to QMK env

--- config.h
+++ config.h
@@ -20,7 +20,7 @@
 #    define RGBLIGHT_HUE_STEP  8
 #    define RGBLIGHT_SAT_STEP  8
 #    define RGBLIGHT_VAL_STEP  8
-#    define RGBLIGHT_LIMIT_VAL 169
+#    define RGBLIGHT_LIMIT_VAL 150
 #endif
 
 // Lets you roll mod-tap keys
