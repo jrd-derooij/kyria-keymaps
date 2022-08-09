4ebc1b0ad4877f251c37b3b4b122db67e0463c3b
jrd-derooij <jrd.derooij@gmail.com>
2022-08-09 16:27:38 +0200

    Auto Commit by copy to QMK env

--- keymap.c
+++ keymap.c
@@ -50,10 +50,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
 	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_MEH, MO(_NAVIGATION),       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
 	),
+    /*
+    * Base Layer: COLEMAK
+    *
+    * ,-------------------------------------------.                              ,-------------------------------------------.
+    * | TAB    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
+    * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+    * | CTL/ESC|   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+    * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
+    * | LSFT   |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
+    * `----------------------+------+------+------+      +      |  |      +      +------+------+------+----------------------'
+    *                        | CTRL | ALT  | GUI  | Space| MEH  |  | Bspc | Symb | CTRL | ALT  | NMBRS|
+    *                        |      |      |      | NAV  |      |  |      |      |      |      |      |
+    *                        `----------------------------------'  `----------------------------------'
+    */
     [_COLEMAK_DH] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSLS,
-     CTL_ESC , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_G ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSLS,
+     CTL_ESC , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_D ,                                        KC_H,   KC_N ,  KC_E ,   KC_I ,  KC_O , KC_QUOT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
 	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_MEH, MO(_NAVIGATION),       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
     ),
 
@@ -86,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, KC_UP, 	_______, _______, 														KC_DOT, 		KC_7, 		KC_8, 		KC_9, 		KC_MINS, 	KC_ASTR, 
 		_______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, 														KC_COMM, 		KC_4, 		KC_5, 		KC_6, 		KC_ENT, 	KC_PLUS, 
 		_______, _______, _______, _______, _______, _______, _______, _______, 			_______, 	_______, 	KC_EQL, 			KC_1, 		KC_2, 		KC_3, 		KC_SLSH, 	_______,
-								   _______, _______, _______, TO(_BASE), _______, 			_______,    KC_0, 	_______, 	KC_DOT, 	_______
+								   _______, _______, _______, TO(_BASE), _______, 			_______,    KC_0, 	_______, 	KC_DOT, 	TO(_NAVIGATION)
 	),
 	[_FUNCTION] = LAYOUT(
 		_______, _______, _______, _______, _______, _______, 														    _______, KC_F9, KC_F10, KC_F11, KC_F12,  _______, 
