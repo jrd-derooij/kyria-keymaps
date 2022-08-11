9f36feb948f0767101035a9fe7cef713e404f576
jrd-derooij <jrd.derooij@gmail.com>
2022-08-11 13:25:21 +0200

    Auto Commit by copy to QMK env

---
+++ custom_keycodes.c
@@ -0,0 +1,17 @@
+// ---- Simple keycodes ---
+
+// Modtaps n stuff
+#define CTL_ESC     MT(MOD_LCTL, KC_ESC)
+#define KC_UNDSC    LSFT(KC_MINUS)
+#define SPC_MEH     MEH_T(KC_SPC)
+#define SPC_NAV     LT(_NAVIGATION, KC_SPC)
+
+// Layer Keys
+#define BASE        DF(_BASE)
+#define COLEMAK     TO(_COLEMAK_DH)
+#define SYMB        MO(_SYMBOLS)
+#define NAVI        MO(_NAVIGATION)
+#define NUMBMO      MO(_NUMBERS)
+#define NUMBTO      TO(_NUMBERS)
+#define FUNC        MO(_FUNCTION)
+#define GAMING      TO(_GAMING)
--- keymap.c
+++ keymap.c
@@ -1,9 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#ifdef CONSOLE_ENABLE
-#include "print.h"
-#endif
-
 enum layers {
 	_BASE = 0,
     _COLEMAK_DH,
@@ -15,11 +11,11 @@ enum layers {
     _GAMING
 };
 
-#define CTL_ESC     MT(MOD_LCTL, KC_ESC)
-#define KC_UNDSC    LSFT(KC_MINUS)
-#define SPC_MEH     MEH_T(KC_SPC)
-#define SPC_NAV     LT(_NAVIGATION, KC_SPC)
+#ifdef CONSOLE_ENABLE
+#include "print.h"
+#endif
 
+#include "custom_keycodes.c" 
 
 enum combos {
     SPCSYM_NAV,
@@ -53,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                                                     KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, 
 	 	CTL_ESC, KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                             KC_H, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
-	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
+	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, SYMB, FUNC, KC_LALT, NUMBTO
 	),
     /*
     * Base Layer: COLEMAK
@@ -73,19 +69,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_G ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSLS,
      CTL_ESC , KC_A ,  LALT_T(KC_R),  LCTL_T(KC_S),   LSFT_T(KC_T) ,   KC_D ,                     KC_H,   RSFT_T(KC_N),  RCTL_T(KC_E),   LALT_T(KC_I),  KC_O , KC_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
-	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
+	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, SYMB, KC_LCTL, KC_LALT, NUMBTO
     ),
 	[_SYMBOLS] = LAYOUT(
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 									 	  			      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   _______, 
 		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,								 	  			      KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_ENT, _______, 
 		_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  _______, _______, 		        _______, _______, KC_UNDSC,KC_LCBR, KC_RCBR, KC_RCBR, _______, _______, 
-						           TO(_COLEMAK_DH), _______, _______, _______, TG(_FUNCTION), 	        _______, _______, _______, _______, TO(_RGB)
+						           COLEMAK, _______, _______, _______, _______, 	        _______, _______, _______, _______, TO(_RGB)
 	),
     [_NAVIGATION] = LAYOUT(
       _______, _______, _______, _______, _______, _______,                                              KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
       _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_BTN1,                                              KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT, KC_INS,
       _______, _______, _______, _______, _______, KC_SLCK, _______, _______,           _______, _______,KC_PAUSE, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, KC_PSCR,
-                                 _______, _______, _______, _______, _______,           _______, _______, _______, _______, TO(_GAMING)
+                                 _______, _______, _______, _______, _______,           _______, _______, _______, _______, GAMING
     ),
      /* Lower Layer: Numpad, Media
     *
@@ -104,25 +100,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, KC_UP, 	_______, _______, 														KC_DOT, 		KC_7, 		KC_8, 		KC_9, 		KC_MINS, 	KC_ASTR, 
 		_______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, 														KC_COMM, 		KC_4, 		KC_5, 		KC_6, 		KC_ENT, 	KC_PLUS, 
 		_______, _______, _______, _______, _______, _______, _______, _______, 			_______, 	_______, 	KC_EQL, 			KC_1, 		KC_2, 		KC_3, 		KC_SLSH, 	_______,
-								   _______, _______, _______, TO(_BASE), _______, 			_______,    KC_0, 	_______, 	KC_DOT, 	TO(_NAVIGATION)
+								   _______, _______, _______, BASE, _______, 			_______,    KC_0, 	_______, 	KC_DOT, 	TO(_NAVIGATION)
 	),
 	[_FUNCTION] = LAYOUT(
 		_______, _______, _______, _______, _______, _______, 														    _______, KC_F9, KC_F10, KC_F11, KC_F12,  _______, 
 		_______, KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,														    _______, KC_F5, KC_F6,	KC_F7,  KC_F8,   _______,  
 		_______, _______, _______, _______, _______, _______, _______, _______, 				      _______, _______, _______, KC_F1, KC_F2, 	KC_F3,  KC_F4,   _______, 
-						   		   _______, _______, _______, TO(_BASE), _______, 				      _______, _______, _______, _______, TO(_BASE)
+						   		   _______, _______, _______, BASE, _______, 				      _______, _______, _______, _______, BASE
 	),
 	[_RGB] = LAYOUT(
 		_______, _______, _______, 	DF(_BASE), 		_______, _______, 													_______, _______, _______, _______, _______, 	_______, 
 		_______, _______, _______, 	DF(_SYMBOLS), 	_______, _______, 													RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, 	_______, 
 		_______, _______, _______, 	DF(_NAVIGATION), _______, _______, _______, _______, 		  	_______, _______, 	_______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD, 	_______, 
-									_______, 		_______, _______, _______, _______, 			_______, _______, 	_______, _______, TO(_BASE)
+									_______, 		_______, _______, _______, _______, 			_______, _______, 	_______, _______, BASE
 	),
     [_GAMING] = LAYOUT(
 		_______, _______, KC_Q, KC_W, KC_E, KC_R, 													_______, _______, _______, _______, _______, 	_______, 
 		_______, _______, KC_A, KC_S, KC_D, KC_F, 													_______, _______, _______, _______, _______, 	_______, 
 		_______, _______, KC_Z, KC_X, KC_C, KC_V, _______, _______, 		  	_______, _______, 	_______, _______, _______, _______, _______, 	_______, 
-									_______, _______, _______, _______, _______, 			_______, _______, 	_______, _______, TO(_BASE)
+									_______, _______, _______, _______, _______, 			_______, _______, 	_______, _______, BASE
     )
 
 };
@@ -191,6 +187,9 @@ bool oled_task_user(void) {
                 break;
             case _FUNCTION:
                 oled_write_P(PSTR("Functions\n"), false);
+                break;
+            case _GAMING:
+                oled_write_P(PSTR("GAMING\n"), false);
                 break;
             default:
                 oled_write_P(PSTR("Undefined\n"), false);
