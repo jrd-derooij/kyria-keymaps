76752447866a31f05ff1892f580792977bca71c7
jrd-derooij <jrd.derooij@gmail.com>
2022-08-11 14:49:09 +0200

    Auto Commit by copy to QMK env

--- keymap.c
+++ keymap.c
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                                                     KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, 
 	 	CTL_ESC, KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                             KC_H, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
-	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, SYMB, FUNC, KC_LALT, NUMBTO
-	),
+	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,                 KC_BSPC, SYMB, FUNC, KC_LALT, NUMBTO
+	),           
     /*
     * Base Layer: COLEMAK
     *
