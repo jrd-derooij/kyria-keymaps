bafdcb60f0c156340540c05f5c058dd986479892
jrd-derooij <jrd.derooij@gmail.com>
2022-08-11 09:16:41 +0200

    Auto Commit by copy to QMK env

--- keymap.c
+++ keymap.c
@@ -17,18 +17,21 @@ enum layers {
 
 #define CTL_ESC     MT(MOD_LCTL, KC_ESC)
 #define KC_UNDSC    LSFT(KC_MINUS)
-#define SPC_MEH     MT(MOD_MEH, KC_SPC)
+#define SPC_MEH     MEH_T(KC_SPC)
 #define SPC_NAV     LT(_NAVIGATION, KC_SPC)
 
 
 enum combos {
     SPCSYM_NAV,
+    COMMDOT_USC,
 }; 
 
 const uint16_t PROGMEM spcsym_combo[] = {SPC_NAV, MO(_SYMBOLS), COMBO_END};
+const uint16_t PROGMEM underscr_combo[] = {KC_COMM, KC_DOTT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     [SPCSYM_NAV] = COMBO(spcsym_combo, MO(_NUMBERS)),
+    [COMMDOT_USC] = COMBO(underscr_combo, KC_UNDSC),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -48,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
 	[_BASE] = LAYOUT(
 		KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                                                     KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, 
-	 	CTL_ESC, KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                                                     KC_H, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), KC_SCLN, KC_QUOT, 
+	 	CTL_ESC, KC_A, LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                             KC_H, RSFT_T(KC_J), RCTL_T(KC_K), LALT_T(KC_L), KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
 	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
 	),
@@ -72,7 +75,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
 	 	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, KC_MEH,       KC_BSPC, MO(_SYMBOLS), KC_LCTL, KC_LALT, TO(_NUMBERS)
     ),
-
 	[_SYMBOLS] = LAYOUT(
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 									 	  			      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   _______, 
 		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,								 	  			      KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_ENT, _______, 
