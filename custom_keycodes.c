8daace835c00f9faf938a497c6abd4d41e4057ce
jrd-derooij <jrd.derooij@gmail.com>
2022-08-17 12:08:39 +0200

    Auto Commit by copy to QMK env

--- custom_keycodes.c
+++ custom_keycodes.c
@@ -15,3 +15,11 @@
 #define NUMBTO      TO(_NUMBERS)
 #define FUNC        MO(_FUNCTION)
 #define GAMING      TO(_GAMING)
+
+// HomeRow Mods
+#define HRM_J       MT(MOD_RSFT, KC_J)
+#define HRM_K       MT(MOD_RCTL, KC_K)
+#define HRM_L       MT(MOD_LALT, KC_L)
+#define HRM_S       MT(MOD_LALT, KC_S)
+#define HRM_D       MT(MOD_LCTL, KC_D)
+#define HRM_F       MT(MOD_LSFT, KC_F)
