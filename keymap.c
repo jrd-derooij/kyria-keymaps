f1bd63b4b6c80016843bdd520dc5c10fcdb65f8f
jrd-derooij <jrd.derooij@gmail.com>
2022-08-21 23:37:57 +0200

    Auto Commit by copy to QMK env

--- keymap.c
+++ keymap.c
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *                        `----------------------------------'  `----------------------------------'
     */
     [_COLEMAK] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_G ,                                          KC_J,  KC_L  ,  KC_U  ,   KC_Y  ,  KC_SCLN, KC_BSLS,
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                          KC_J,  KC_L  ,  KC_U  ,   KC_Y  ,  KC_SCLN, KC_BSLS,
      CTL_ESC , KC_A ,  HRM_Rc ,  HRM_Sc,   HRM_Tc,  KC_D ,                                          KC_H,  HRM_Nc,  HRM_Ec,   HRM_Ic,  KC_O , KC_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K, KC_M, KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , XXXXXXX,XXXXXXX,     XXXXXXX  , XXXXXXX, KC_K, KC_M, KC_COMM, KC_DOT ,KC_SLSH, KC_MINS,
 	 	        COLEMAKDH, KC_LALT, KC_LGUI, SPC_NAV, KC_LEAD,       KC_BSPC, SYMB, FUNC, KC_MEH, NUMBTO
     ),
     [_COLEMAK_DH] = LAYOUT(
