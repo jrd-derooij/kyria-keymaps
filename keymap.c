a6e93a01cce0ea5133c81549414a8e57e427197b
jrd-derooij <jrd.derooij@gmail.com>
2022-09-19 13:29:03 +0200

    Auto Commit by copy to QMK env

--- keymap.c
+++ keymap.c
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, KC_Q, KC_W, KC_E, KC_R, 													            KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, 
 		_______, _______, KC_A, KC_S, KC_D, KC_F, 													            KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 		_______, _______, KC_Z, KC_X, KC_C, KC_V, _______, _______, 		  	            _______, _______, 	KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS, 
-									_______, _______, _______, _______, _______, 			_______, _______, 	_______, BASE2, BASE
+									_______, _______, _______, _______, _______, 			_______, _______, 	_______, _______, BASE
     )
 };
 
