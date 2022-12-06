21dce3ba98dc144cfe42c6f3581bb333c8a6b53f
jrd-derooij <jrd.derooij@gmail.com>
2022-12-06 17:14:39 +0100

    Auto Commit by copy to QMK env

--- keymap.c
+++ keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * | LSFT   |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
     * `----------------------+------+------+------+      +      |  |      +      +------+------+------+----------------------'
     *                        | CTRL | ALT  | GUI  | Space| LEAD |  | Bspc | Symb | FUNC | MEH  | NMBRS|  
-    *                        |      |      |      | MEH  |      |  |      |      |      |      |      |
+    *                        |      |      |      | NAV  |      |  |      |      |      |      |      |
     *                        `----------------------------------'  `----------------------------------'
     */
 	[_BASE] = LAYOUT(
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//  	        KC_LCTL, KC_LALT, KC_LGUI, SPC_NAV, _______,                    KC_BSPC, SYMB, FUNC, KC_MEH, NUMBTO
     // ),
 	[_SYMBOLS] = LAYOUT(
-		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 									 	  			      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   _______, 
+		KC_TILDE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 									 	  			      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   _______, 
 		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,								 	  			      KC_LBRC, KC_LPRN, KC_RPRN, KC_SCLN, KC_ENT, _______, 
-		_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  _______, _______, 		        _______, _______, KC_RBRC,KC_LCBR, KC_RCBR, KC_RCBR, KC_QUES, _______, 
-						           COLEMAK, _______, _______, _______, _______, 	        _______, _______, _______, _______, TO(_RGB)
+		_______, KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_EQL,  _______, _______, 		        _______, _______, KC_RBRC,KC_LCBR, KC_RCBR, KC_RCBR, KC_QUES, KC_GRV, 
+						           MAGIC_TOGGLE_CTL_GUI, _______, _______, _______, _______, 	        _______, _______, _______, _______, TO(_RGB)
 	),  
     [_NAVIGATION] = LAYOUT(
       _______, _______, _______, KC_WBAK, KC_WFWD, _______,                                              KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
